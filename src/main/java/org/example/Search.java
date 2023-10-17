e395f5a158102d48551e2ecaa91204dc2c222986
hayeong120 <jbg01200249@gmail.com>
2023-10-18 08:41:53 +0900

    밑패널 완성

--- src/main/java/org/example/Search.java
+++ src/main/java/org/example/Search.java
@@ -9,7 +9,7 @@ public class Search {
         frame.setPreferredSize(new Dimension(1200,700));    // 폭과 넓이 적용
         frame.setLocation(180,50);      // 윈도우 창의 나타낼 위치
         frame.setLayout(null);
-        //frame.setResizable(false);      // 창 크기 제한
+        frame.setResizable(false);      // 창 크기 제한
         frame.setVisible(true);
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -41,23 +41,27 @@ public class Search {
 
         JPanel p1 = new JPanel();
         p1.setBackground(color1);
-        p1.setBounds(0, 550, 1200, 110);
-        //p1.setLayout(null);
-        //p1.setLayout(new BoxLayout(p1, BoxLayout.X_AXIS));
-        p1.add(Box.createHorizontalStrut(10));
+        //p1.setBounds(0, 550, 1200, 100);
+        p1.setSize(1200,100);
+        p1.add(Box.createHorizontalStrut(5));
         p1.add(b1);
         p1.add(Box.createHorizontalStrut(400));
         p1.add(b2);
         p1.add(Box.createHorizontalStrut(400));
         p1.add(b3);
-        p1.add(Box.createHorizontalStrut(20));
+        p1.add(Box.createHorizontalStrut(10));
 
         JPanel p2 = new JPanel();
         p2.setBackground(color2);
-        p2.setBounds(0, 580, 1200, 10);
-        //p2.setLayout(null);
+        p2.setSize(1200,10);
+        //p2.setBounds(0, 540, 1200, 10);
 
-        frame.add(p1);
-        frame.add(p2);
+        JPanel p3 = new JPanel();
+        p3.setBounds(0,555,1200,110);
+        p3.setLayout(new BoxLayout(p3, BoxLayout.Y_AXIS));
+        p3.add(p2);
+        p3.add(p1);
+
+        frame.add(p3);
     }
 }
